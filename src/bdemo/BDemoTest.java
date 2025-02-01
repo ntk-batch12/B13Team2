6e6cded756f8041f301ebc6626bddd5f1247ee72
ivancollazoo <collazoivan1412@gmail.com>
2025-01-31 22:40:44 -0400

    Wrote my code

--- src/bdemo/BDemoTest.java
+++ src/bdemo/BDemoTest.java
@@ -1,23 +1,32 @@
 package bdemo;
 
 public class BDemoTest {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Team 2");
-		
+
+		// Eric's comment
 		int a = 3;
 		int b = 5;
 		System.out.println(a + b);
 	}
-	//Eric's comment
 	
-	
-	
-	
-	//implemented a new method called add()
-	//Adriana
-	
-	public void add(int a, int b ){
-		System.out.println(a+b);
+
+	// implemented a new method called add()
+	// Adriana
+
+	public void add(int a, int b) {
+		System.out.println(a + b);
+
+		// If statement
+		// Ivan
+
+		int team2 = 1000;
+		int otherTeams = 0;
+
+		if (team2 > otherTeams) {
+			System.out.println("WE THE BEST");
+		}
+
 	}
 }
